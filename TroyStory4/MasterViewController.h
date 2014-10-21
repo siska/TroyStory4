eb9b68e3ad97792fa4105fc01f5743767ee8d1c8
Ryan Siska <rsiska1@gmail.com>
2014-10-21 12:13:50 -0500

    Post Step 6

--- TroyStory4/MasterViewController.h
+++ TroyStory4/MasterViewController.h
@@ -16,3 +16,26 @@
 
 @end
 
+/*
+step 1 
+ - we just set up the project to be able to use Core Data and recreate how Core Data is used - one important thing was to check the Core Data box when we were setting up the program
+ - we also deleted one @property from this .h file and deleted everything from MasterVC, adding back the #import, interface, and implemenation - nothing added yet
+
+ step 2
+ - we put a text field in the navigation bar - important thing here was that we changed the title of the navigation by opening up the right side panel and changing "prompt"
+ - we then created an IBAction for the text field - important thing here was that instead of creating a delegate for it, we just hooked up the "did end on exit" from the right side column to the IBAction - from what Max said, this can also be used to switch from one text fielx to anoterh
+
+ step 3
+ - normal processes - we created the trojans array and added the <UITableViewDataSource> - we then added the cellForRow... and numberOfRows... - the only new thing we did here related to Core Data was to add the ManagedObject which gives back an object from the trojans array at a specific index - we haven't gone too much into this yet
+
+step 4
+ - start to where we're using the Core Data - opened the TroyStory4.xcdatamodeld folder and added a new entity of Trojan - don't make plural because it implies that they're all plural - then we added the viewDidLoad and loadData - the parts within loadData are pulling the data from that Trojan thing we set in Core Data and then set that to the array that we created above, then we reloaded the data of the tableview so it shows up
+ - also had to reset the simulator via iOS Simulator - Reset Content and Settings or get an error
+ 
+ step 5
+ - add the data pieces within the onTrojanConquest - this adds a Trojan object that we created in the .xcdata... with whatever name was in the text field - we then save to the managedObjectContext and call the loadData to reload the tableView data with the new name
+ 
+ step 6
+ - we went into the cxdatamodel again and added a new attribute of "prowess" to the Trojan - set as an integer 32 or something - he said this is the standard - then we added that to the onTrojanConquest so that the trojans will get a random prowess number assigned to them
+
+*/
--- TroyStory4/MasterViewController.m
+++ TroyStory4/MasterViewController.m
@@ -1,9 +1,49 @@
 #import "MasterViewController.h"
-@interface MasterViewController()
+
+@interface MasterViewController() <UITableViewDataSource>
+@property NSArray *trojans; //step 3
+
 @end
 
 @implementation MasterViewController
 
+-(IBAction)onTrojanConquest:(UITextField *)sender
+{ //step 5 - everything down to the resignFirstResponder
+    NSManagedObject *trojan = [NSEntityDescription insertNewObjectForEntityForName:@"Trojan" inManagedObjectContext:self.managedObjectContext];
+    [trojan setValue:sender.text forKey:@"name"];
+    [trojan setValue:@(arc4random_uniform(10)+1) forKey:@"prowess"]; //step 6
+    [self.managedObjectContext save:nil]; //the save is the error state - don't set to nil in an actual app
+    [self loadData];
 
+    sender.text = @"";
+    [sender resignFirstResponder];
+}
+
+-(void)viewDidLoad //step 4
+{
+    [self loadData];
+}
+
+-(void)loadData //step 4
+{
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"Trojan"];
+    self.trojans = [self.managedObjectContext executeFetchRequest:request error:nil]; //step 4 - don't use nil here in actual apps, do something with the error NSError(error) etc.
+    [self.tableView reloadData];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSManagedObject *trojan = [self.trojans objectAtIndex:indexPath.row];
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    cell.textLabel.text = [trojan valueForKey:@"name"];
+    cell.detailTextLabel.text = [[trojan valueForKey:@"prowess"] stringValue]; //step 6
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.trojans.count;
+}
 
 @end
